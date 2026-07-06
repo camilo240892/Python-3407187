e3cc4a45a529377a713c3e24879597604912b0e0
Camilo Berdugo <camilo.berdugo24@gmail.com>
2026-07-06 09:29:29 -0500

    sqlmodel con creacion de clientes y listar ttodos

--- app/enrutadores/clientes.py
+++ app/enrutadores/clientes.py
@@ -2,14 +2,16 @@ from fastapi import APIRouter, HTTPException
 from ..modelos.clientes import Cliente, ClienteCrear, ClienteEditar
 from ..listas import lista_clientes
 from ..conexion_bd import Sesion_dependencia
+from sqlmodel import select
 
 rutas_clientes = APIRouter()
 #lista_clientes: list[Cliente] = []
 
 # endpoint, para obtener o listar todos los clientes
 @rutas_clientes.get("/clientes", response_model=list[Cliente])
-async def listar_clientes():
-    return lista_clientes
+async def listar_clientes(sesion: Sesion_dependencia):
+    lista_cli= sesion.exec(select(Cliente)).all()
+    return lista_cli
 
 
 # endpoint, para obtener o listar un solo cliente de la lista
@@ -30,7 +32,7 @@ async def crear_cliente(datos_cliente: ClienteCrear, mi_sesion: Sesion_dependenc
     cliente_val = Cliente.model_validate(datos_cliente.model_dump())
     mi_sesion.add(cliente_val)
     mi_sesion.commit()
-    mi_sesion.refresh()
+    mi_sesion.refresh(cliente_val)
     return cliente_val
 
 # endpoint, para editar un cliente, y agregar a la lista
